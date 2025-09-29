a26c85819a312d491d020f5b4a13cd8cd52bd71c
rahul-ofis <rahul.m@alphanimble.com>
2025-09-29 06:56:26 +0530

    first commit

--- packages/twenty-front/src/modules/auth/sign-in-up/hooks/useSignInUpForm.ts
+++ packages/twenty-front/src/modules/auth/sign-in-up/hooks/useSignInUpForm.ts
@@ -57,8 +57,8 @@ export const useSignInUpForm = () => {
     }
 
     if (isDeveloperDefaultSignInPrefilled === true) {
-      form.setValue('email', prefilledEmail ?? 'tim@apple.dev');
-      form.setValue('password', 'tim@apple.dev');
+      form.setValue('email', prefilledEmail ?? 'gbo@gbo.com');
+      form.setValue('password', 'gbo@gbo.com');
     }
   }, [form, isDeveloperDefaultSignInPrefilled, prefilledEmail]);
   return { form: form };
